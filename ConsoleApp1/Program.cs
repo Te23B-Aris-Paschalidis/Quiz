cdeaa97c425abce2365db7789efd4f95728c0e08
Aris Paschalidis <aris.paschalidis@elev.ga.ntig.se>
2024-09-11 15:04:33 +0200

    wdadsazfafwafwasfsa

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿String name = "Player 1";
+int score = 0;
+
+
+score += AskQuestion("Vilken är den farligaste björnen i världen?", "A) Isbjörn B) BrunBjörn C) GrizzlyBjörn", "C");
+Console.WriteLine($"DU HAR {score} POÄNG!!! ");
+score += AskQuestion("Vilken är den farligaste björnen i världen?", "A) Isbjörn B) BrunBjörn C) GrizzlyBjörn", "C");
+
+
+Console.ReadLine();
+
+
+static int AskQuestion(string question, string answers, string correct)
+{
+
+    Console.WriteLine(question);
+
+    Console.WriteLine("\n");
+
+    Console.WriteLine(answers);
+    string answer = Console.ReadLine();
+
+    if (answer.ToUpper() == correct)
+    {
+        Console.Write("RÄTT!! ");
+        return 100;
+    }
+    else
+    {
+        Console.WriteLine("Fel Nästa fråga.");
+        Console.WriteLine("\n");
+        return 0;
+    }
+}
